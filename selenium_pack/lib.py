7b36d6f317b53b6a2825c4b1b743381ab01d01b4
our ame <you@example.com>
2023-02-01 21:30:17 +0300

    init

--- selenium_pack/lib.py
+++ selenium_pack/lib.py
@@ -1,7 +1,6 @@
 import pathlib
 import pickle
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
